9862d55b7ff7f3d961aeac2307920827deefedca
Robso <robso.crlira@gmail.com>
2020-02-18 11:55:00 -0300

    Introdução á Design Patterns com python

---
+++ calculate_shipping.py
@@ -0,0 +1,11 @@
+from shippings import calculate_default, calculate_express
+
+
+class CalculateShipping(object):
+
+    def execute_calculation(self, order, shipping):
+        if shipping == 'Default':
+            total = order.value * 0.05
+        elif shipping == 'Express':
+            total = order.value * 0.1
+        print(total)
---
+++ shippings.py
@@ -0,0 +1,10 @@
+class Default(object):
+
+    def calculate_default(order):
+        total = order.value * 0.05
+
+
+class Express(object):
+
+    def calculate_express( order):
+        total = order.value * 0.1
